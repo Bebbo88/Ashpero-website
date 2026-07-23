1bbff1ae09804a4b43064570b26e6fcb23cec52f
Ahmed Tarek <bebboxdd@gmail.com>
2026-07-23 14:59:29 +0300

    implement meta pixel code

--- src/app/layout.js
+++ src/app/layout.js
@@ -76,20 +76,48 @@ export default async function RootLayout({ children }) {
         <Script id="theme-init" strategy="beforeInteractive">
           {THEME_INIT_SCRIPT}
         </Script>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              
+              fbq('init', '1347029870717903'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${montserrat.variable} ${playfair.variable} antialiased`}
         suppressHydrationWarning
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1347029870717903&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <LangProvider initialLocale={locale} dictionary={dictionary}>
           <ReduxProvider>
             <ReactQueryProvider>
               <AuthSessionProvider>
                 <ModeProvider>
                   <CartDrawerProvider>
-                    <SplashScreen hasSeenSplash={hasSeenSplash}>
-                      {children}
-                    </SplashScreen>
+                    {/* <SplashScreen hasSeenSplash={hasSeenSplash}> */}
+                    {children}
+                    {/* </SplashScreen> */}
                     <FloatingActions />
                     <CartDrawer />
                   </CartDrawerProvider>
