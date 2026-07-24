df4fd8b381c6cf6722dda98e070ad6de815a5a5a
Ahmed Tarek <bebboxdd@gmail.com>
2026-07-24 19:02:41 +0300

    feat: implement Meta Pixel events tracking

--- src/components/product/ProductDetailsPage.jsx
+++ src/components/product/ProductDetailsPage.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useProductDetailsPageLogic } from "./ProductDetailsPage.logic";
 import { ProductDetailsPageUI } from "./ProductDetailsPage.ui";
 
