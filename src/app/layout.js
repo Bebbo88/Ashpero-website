97e2c2d1fa1d3f423aa64c3c3243860dfdea2864
Ahmed Tarek <128825083+Bebbo88@users.noreply.github.com>
2026-08-18 10:46:58 +0300

    Add Microsoft Clarity tracking script to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -124,6 +124,16 @@ export default async function RootLayout({ children }) {
             </ReactQueryProvider>
           </ReduxProvider>
         </LangProvider>
+
+              <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "هنا_الـ_ID_بتاع_العميل");
+          `}
+        </Script>
       </body>
     </html>
   );
